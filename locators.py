4afb7347c736e1b48a4988cfe8fb00a833a732a3
Sadpotato <alexprgrigoriev@gmail.com>
2020-03-30 01:54:30 +0400

    Selenium scraper finished

--- locators.py
+++ locators.py
@@ -5,7 +5,11 @@ class MainPageLocators(object):
     FIND_MATCHING_STRING_BUTTON = (By.ID, 'submit1')
     DOWNLOAD_SIGN = (By.CLASS_NAME, 'pzz4') # used to verify that page is loaded
     FIND_MATCHING_STRING = (By.ID, 'p')
+    WORD_CONTEXT = (By.ID, 'url1')
+    CONTEXT_SEGMENT = (By.ID, 'mycell3')
+    SEARCH_RESULT_FREQ = (By.CLASS_NAME, 'auto-style2')
+    
 
 class SearchResultsPageLocators(object):
     """A class for search results locators. All search results locators should come here"""
-    pass
+    MORE_CONTEXT_LABEL = (By.ID, 'w_moreContext')
--- page.py
+++ page.py
@@ -1,11 +1,16 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from element import BasePageElement
 from locators import MainPageLocators
+from locators import SearchResultsPageLocators
+from random import randint
+from time import sleep
+from time import time
+import csv
 
 class SearchTextElement(BasePageElement):
     """This class gets the search text from the specified locator"""
-
     #The locator for search box where search string is entered
     locator = MainPageLocators.FIND_MATCHING_STRING
 
@@ -15,6 +20,14 @@ class BasePage(object):
 
     def __init__(self, driver):
         self.driver = driver
+        
+    def frame_switch(self, name_selector):
+        WebDriverWait(self.driver, 30).until(
+            EC.presence_of_element_located((By.NAME, name_selector)))
+        self.driver.switch_to.frame(self.driver.find_element_by_name(name_selector))
+        
+    def return_to_default_frame(self):
+        self.driver.switch_to.parent_frame()
 
 
 class MainPage(BasePage):
@@ -26,11 +39,8 @@ class MainPage(BasePage):
     def verify_loaded(self):
         WebDriverWait(self.driver, 30).until(
             EC.visibility_of_element_located(MainPageLocators.DOWNLOAD_SIGN))
-        element = self.driver.find_element(*MainPageLocators.DOWNLOAD_SIGN)
-        print(element.text)
-        
-    def frame_switch(self, name_selector):
-            self.driver.switch_to.frame(self.driver.find_element_by_name(name_selector))
+        #element = self.driver.find_element(*MainPageLocators.DOWNLOAD_SIGN)
+        #print(element.text)
         
     def __init__(self, driver):
         super().__init__(driver)
@@ -45,12 +55,60 @@ class MainPage(BasePage):
         """Triggers the search"""
         element = self.driver.find_element(*MainPageLocators.FIND_MATCHING_STRING_BUTTON)
         element.click()
-
+        
+    def click_switch_to_context(self):
+        self.return_to_default_frame()
+        self.frame_switch('x2')
+        WebDriverWait(self.driver, 30).until(
+            EC.visibility_of_element_located(MainPageLocators.WORD_CONTEXT))
+        element = self.driver.find_element(*MainPageLocators.WORD_CONTEXT)
+        element.click()
+        self.return_to_default_frame()
+        
 
 class SearchResultsPage(BasePage):
     """Search results page action methods come here"""
-
-    def is_results_found(self):
-        # Probably should search for this text in the specific page
-        # element, but as for now it works fine
-        return "No results found." not in self.driver.page_source
+    
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.frame_switch('x3')
+        WebDriverWait(self.driver, 30).until(
+            EC.visibility_of_element_located(SearchResultsPageLocators.MORE_CONTEXT_LABEL))
+        
+    def cycle_through_pages(self, pages_count):
+        with open('datares' + str(time()) + '.csv', 'w', newline='') as csvfile:
+            context_writer = csv.writer(csvfile, delimiter='^',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            
+            for i in range(pages_count):
+                self.get_data(context_writer)
+                self.go_and_wait()
+        
+    def get_data(self, writer):
+        try:
+            for element in self.driver.find_elements_by_tag_name('tr')[2:]:
+                row = []
+                for cell in element.find_elements_by_tag_name('td'):
+                    row.append(cell.text)
+                writer.writerow(row)
+        finally:
+            return
+    
+    def go_and_wait(self):
+        navigation_arrow = self.driver.find_elements_by_tag_name('td')[0].find_elements_by_tag_name('a')[6]
+        navigation_arrow.click()
+        
+        self.return_to_default_frame() # Now gow to main frame
+        random_time = randint(3, 6)    # Wait random time. The page should reload
+        print('i`m waiting', random_time, 'seconds')
+        sleep(random_time)
+        self.frame_switch('x3')
+        pass
+        
+        
+    
+    
+    
+    
+    
+    
